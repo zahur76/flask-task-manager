1398f8a6db3135a90dda2099274bca50dd097f75
zahur76 <zahurmeerun@hotmail.com>
2020-11-05 04:17:06 +0000

    Add category dropdown in add_task

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def logout():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("add_task.html")
+    categories = mongo.db.catogories.find().sort("category_name", 1)
+    return render_template("add_task.html", categories=categories)
 
 
 if __name__ == "__main__":
